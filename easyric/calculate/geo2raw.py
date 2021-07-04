fix wrong geo distance calculation

easyric/calculate/geo2raw.py
@@ -176,31 +176,34 @@ def get_img_coords_dict(param, points, method='pmat', distort_correct=True, igno
     return out_dict
 
 
-def filter_cloest_img(p4d, img_dict, num=3):
+def filter_cloest_img(p4d, img_dict, plot_geo, num=3):
     dist_pix = []
     dist_geo = []
     dist_name = []
     
     img_dict_sort = {}
     for img_name, img_coord in img_dict.items():
-        xmin, ymin = img_coord.min(axis=0)
-        xmax, ymax = img_coord.max(axis=0)
-        xctr = (xmax-xmin) / 2
-        yctr = (ymax-ymin) / 2
+        #xmin_pix, ymin_pix = img_coord.min(axis=0)
+        #xmax_pix, ymax_pix = img_coord.max(axis=0)
+        #xctr_pix = (xmax_pix + xmin_pix) / 2
+        #yctr_pix = (ymax_pix + ymin_pix) / 2
 
-        xctr_geo, yctr_geo = geotiff.pixel2geo(np.asarray([[xctr, yctr]]), p4d.dom_header)[0,:]
+        xmin_geo, ymin_geo = plot_geo[:,0:2].min(axis=0)
+        xmax_geo, ymax_geo = plot_geo[:,0:2].max(axis=0)
+        xctr_geo = (xmax_geo + xmin_geo) / 2
+        yctr_geo = (ymax_geo + ymin_geo) / 2
 
-        img_xgeo, img_ygeo, _ = p4d.img[img_name].cam_pos
+        ximg_geo, yimg_geo, _ = p4d.img[img_name].cam_pos
 
         w, h = p4d.img[img_name].w, p4d.img[img_name].h
 
         x1, y1 = 0.5 * w, 0.5 * h
 
-        dist_pix.append(np.sqrt((x1 - xctr) ** 2 + (y1 - yctr) ** 2) / np.sqrt((w) ** 2 + (h) ** 2) * 100)
-        dist_geo.append(np.sqrt((img_xgeo-xctr_geo) ** 2 + (img_ygeo - yctr_geo) ** 2))
+        #dist_pix.append(np.sqrt((x1 - xctr_pix) ** 2 + (y1 - yctr_pix) ** 2) / np.sqrt((w) ** 2 + (h) ** 2) * 100)
+        dist_geo.append(np.sqrt((ximg_geo-xctr_geo) ** 2 + (yimg_geo - yctr_geo) ** 2))
         dist_name.append(img_name)
 
-    dist_pix_idx = (np.asarray(dist_pix)).argsort()[:num]
+    #dist_pix_idx = (np.asarray(dist_pix)).argsort()[:num]
     dist_geo_idx = (np.asarray(dist_geo)).argsort()[:num]
     
     img_dict_sort = {dist_name[idx]:img_dict[dist_name[idx]] for idx in dist_geo_idx}

easyric/io/geotiff.py
@@ -281,10 +281,15 @@ def mean_values(geotiff_path, polygon='all', geo_head=None):
 
 
 def percentile_values(data, percentile=5):
-    return np.nanmean(data[data < np.nanpercentile(data, percentile)])
+    if percentile < 50:
+        v = np.nanmean(data[data < np.nanpercentile(data, percentile)])
+    else:
+        v = np.nanmean(data[data > np.nanpercentile(data, percentile)])
+
+    return v
 
 
-def min_values(geotiff_path, polygon='all', geo_head=None):
+def min_values(geotiff_path, polygon='all', geo_head=None, pctl=5):
     """
     :param geotiff_path:
     :param polygon:
@@ -306,14 +311,14 @@ def min_values(geotiff_path, polygon='all', geo_head=None):
         if polygon == 'all':
             data = tif.asarray().astype(float)
             data[data == geo_head['nodata']] = np.nan
-            z_min = percentile_values(data)
+            z_min = percentile_values(data, pctl)
         else:
             if isinstance(polygon, np.ndarray):
                 roi = geo2pixel(polygon, geo_head)   # roi = (horizontal, vertical)
                 # [TODO] only dsm supported
                 #imarray, offsets = imarray_clip(data, roi)
                 imarray, _, _ = crop_by_coord(geotiff_path, roi, buffer=0, ts=ts, tif=tif)
-                z_min = percentile_values(imarray)
+                z_min = percentile_values(imarray, pctl)
             elif isinstance(polygon, list):
                 z_min = []
                 total_num = len(polygon)
@@ -322,11 +327,11 @@ def min_values(geotiff_path, polygon='all', geo_head=None):
                         roi = geo2pixel(poly, geo_head)
                         #imarray, offsets = imarray_clip(data, roi)
                         imarray, _, _ = crop_by_coord(geotiff_path, roi, buffer=0, ts=ts, tif=tif)
-                        z_min.append(percentile_values(imarray))
+                        z_min.append(percentile_values(imarray, pctl))
                     else:
                         raise TypeError('Only numpy.ndarray points itmes in the list are supported')
 
-                print(f"[io][geotiff][mean] Reading DSM clippers | {i+1}/{total_num}", end="\r")
+                    print(f"[io][geotiff][mean] Reading DSM clippers | {i+1}/{total_num}", end="\r")
             else:
                 raise TypeError('Only numpy.ndarray points list are supported')
 
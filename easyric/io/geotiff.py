b3391e76c35bb8b8d3c758a48f0f1c044f6f8985
Howcanoe WANG <howcanoewang@gmail.com>
2021-10-20 21:17:39 +0900

    rm skimage.tifffile = 0.16 dependency

--- easyric/io/geotiff.py
+++ easyric/io/geotiff.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pyproj
-from skimage.external import tifffile
+import skimage
 from skimage.draw import polygon
 from pyproj.exceptions import CRSError
 
@@ -8,110 +8,34 @@ from easyric import caas_lite
 import tifffile as tf
 
 
-def _get_header_string(geotiff_path):
-    with tifffile.TiffFile(geotiff_path) as tif:
-        # >>> print(tif.info())
-        '''
-        TIFF file: broccoli_tanashi_5_20191008_mavicRGB_15m_M_dsm.tif, 274 MiB, little endian
+def get_header(tif_path):
+    with tf.TiffFile(tif_path) as tif:
+        header = {'width': None, 'length': None, 'dim':1, 
+                'scale': None, 'tie_point': None, 'nodata': None, 'proj': None}
 
-        Series 0: 19866x13503, float32, YX, 1 pages, not mem-mappable
-
-        Page 0: 19866x13503, float32, 32 bit, minisblack, lzw
-        * 256 image_width (1H) 13503
-        * 257 image_length (1H) 19866
-        * 258 bits_per_sample (1H) 32
-        * 259 compression (1H) 5
-        * 262 photometric (1H) 1
-        * 273 strip_offsets (19866I) (159344, 159734, 160140, 160546, 160952, 161359, 1
-        * 277 samples_per_pixel (1H) 1
-        * 278 rows_per_strip (1H) 1
-        * 279 strip_byte_counts (19866I) (390, 406, 406, 406, 407, 410, 412, 414, 416,
-        * 284 planar_configuration (1H) 1
-        * 305 software (12s) b'pix4dmapper'
-        * 317 predictor (1H) 3
-        * 339 sample_format (1H) 3
-        * 33550 model_pixel_scale (3d) (0.0029700000000000004, 0.0029700000000000004, 0
-        * 33922 model_tie_point (6d) (0.0, 0.0, 0.0, 368090.77975000005, 3956071.13823,
-        ******** some case ********* (0.0, 0.0, 0.0, 368090.77975000005, 3956071.13823, 0
-        * 34735 geo_key_directory (32H) (1, 1, 0, 7, 1024, 0, 1, 1, 1025, 0, 1, 1, 1026
-        * 34737 geo_ascii_params (30s) b'WGS 84 / UTM zone 54N|WGS 84|'
-        * 42113 gdal_nodata (7s) b'-10000'
-
-        ====================
-        but in some case, it will has some series like:
-
-        Series 1: 3107x3989, float32, YX, 1 pages, not mem-mappable
-
-        Page 1: 3107x3989, float32, 32 bit, minisblack, lzw, reduced|tiled
-        * 254 new_subfile_type (1I) 1
-        * 256 image_width (1H) 3989
-        * 257 image_length (1H) 3107
-        * 258 bits_per_sample (1H) 32
-        * 259 compression (1H) 5
-        * 262 photometric (1H) 1
-        ...
-        '''
-        return tif.info()
-
-
-def _prase_header_string(geotiff_string):
-    header = {'width': None, 'length': None, 'scale': None, 'tie_point': None, 'nodata': None, 'proj': None}
-
-    for line in geotiff_string.split('\n'):
-        # just load page 0 contents
-        if "Series 1" in line:
-            break
-
-        if '*' in line:
-            line_sp = line.split(' ')
-            code = line_sp[1]
-            if code == '256':
-                # * 256 image_width (1H) 13503
-                header['width'] = int(line_sp[-1])
-            elif code == '257':
-                # * 257 image_length (1H) 19866
-                header['length'] = int(line_sp[-1])
-            elif code == '33550':
-                # * 33550 model_pixel_scale (3d) (0.0029700000000000004, 0.0029700000000000004, 0
-                x = float(line_sp[-3][1:-1])
-                y = float(line_sp[-2][:-1])
-                header['scale'] = (x, y)
-            elif code == '33922':
-                # * 33922 model_tie_point (6d) (0.0, 0.0, 0.0, 368090.77975000005, 3956071.13823,
-                x = float(line_sp[7][:-1])
-                y = float(line_sp[8][:-1])
-                header['tie_point'] = (x, y)
-            elif code == '34737':
-                # * 34737 geo_ascii_params (30s) b'WGS 84 / UTM zone 54N|WGS 84|'
-                # * 34737 geo_ascii_params (7s) b'WGS-84'
-                # * 34737 geo_ascii_params (30s) b'WGS 84|WGS 84 / UTM zone 54N|'
-                bstring = max(line.split('34737')[-1].split(") b'")[-1].split('|'), key=len)
-                #bstring = line.split('34737')[-1].split(") b'")[-1].split('|')[0]
-                # in WGS 84|WGS 84 / UTM zone 54N| -> WGS 84, will give a wrong result, so pick the longest string instead
-                # url: https://www.kite.com/python/answers/how-to-find-the-longest-string-in-a-list-in-python
-                print(f'[io][geotiff][GeoCorrd] Comprehense [{line}] to geotiff coordinate tag [{bstring}]')
-                try:
-                    proj = pyproj.CRS.from_string(bstring)
-                    header['proj'] = proj
-                except CRSError as e:
-                    print(f'[io][geotiff][GeoCorrd] Generation failed, because [{e}], but you can manual specify it later by \n'
-                          '>>> import pyproj \n'
-                          '>>> proj = pyproj.CRS.from_epsg() # or from_string() or refer official documents:\n'
-                          'https://pyproj4.github.io/pyproj/dev/api/crs/coordinate_operation.html')
-                    pass
-            elif code == '42113':
-                header['nodata'] = int(line_sp[-1][2:-1])
-            else:
-                pass
-        else:
-            continue
-
-    return header
-
-
-def get_header(geotiff_path):
-    geotiff_string = _get_header_string(geotiff_path)
-    header = _prase_header_string(geotiff_string)
+        header["length"] = tif.pages[0].shape[0]
+        header["width"] = tif.pages[0].shape[1]
+        if len(tif.pages[0].shape) > 2:
+            header["dim"] = tif.pages[0].shape[2] 
+        header["nodata"] = tif.pages[0].nodata
+        
+        # tif.pages[0].geotiff_tags >>> 'ModelPixelScale': [0.0034900000000000005, 0.0034900000000000005, 0.0]
+        header["scale"] = tif.pages[0].geotiff_tags["ModelPixelScale"][0:2]
+        
+        # tif.pages[0].geotiff_tags >>> 'ModelTiepoint': [0.0, 0.0, 0.0, 419509.89816000004, 3987344.8286, 0.0]
+        header["tie_point"] = tif.pages[0].geotiff_tags["ModelTiepoint"][3:5]
+        
+        # tif.pages[0].geotiff_tags >>> 'GTCitationGeoKey': 'WGS 84 / UTM zone 54N'
+        proj_str = tif.pages[0].geotiff_tags["GTCitationGeoKey"]
+        try:
+            proj = pyproj.CRS.from_string(proj_str)
+            header['proj'] = proj
+        except CRSError as e:
+            print(f'[io][geotiff][GeoCorrd] Generation failed, because [{e}], but you can manual specify it later by \n'
+                    '>>> import pyproj \n'
+                    '>>> proj = pyproj.CRS.from_epsg() # or from_string() or refer official documents:\n'
+                    'https://pyproj4.github.io/pyproj/dev/api/crs/coordinate_operation.html')
+            pass
 
     return header
 
@@ -128,11 +52,8 @@ def get_imarray(geotiff_path, geo_head=None):
     -------
 
     """
-    with tifffile.TiffFile(geotiff_path) as tif:
-        if geo_head is None:
-            geo_head = _prase_header_string(tif.info())
-        data = tif.asarray().astype(float)
-        data[data == geo_head['nodata']] = np.nan
+    with tf.TiffFile(geotiff_path) as tif:
+        data = tif.pages[0].asarray()
 
     return data
 
@@ -167,13 +88,7 @@ def point_query(geotiff, point_hv, geo_head=None, mode="full"):
     is_geo = True
     if not isinstance(geotiff, str):
         if mode == "full":
-            with tifffile.TiffFile(geotiff) as tif:
-                # equal to get_header()
-                if geo_head is None:
-                    geo_head = _prase_header_string(tif.info())
-                # equal to get_imarray()
-                data = tif.asarray().astype(float)
-                data[data == geo_head['nodata']] = np.nan
+            data = get_imarray(geotiff)
         else:
             ts = caas_lite.TiffSpliter(geotiff, 2000, 2000)
             tif = tf.TiffFile(ts.tif_path)
@@ -238,21 +153,14 @@ def mean_values(geotiff_path, polygon='all', geo_head=None):
     :param geo_head:
     :return:
     """
-    with tifffile.TiffFile(geotiff_path) as tif:
-        # equal to get_header()
-        if geo_head is None:
-            geo_head = _prase_header_string(tif.info())
-        # equal to get_imarray()
-        #data = tif.asarray().astype(float)
-        #data[data == geo_head['nodata']] = np.nan
+    with tf.TiffFile(geotiff_path) as tif:
+        geo_head = get_header(geotiff_path)
             
         # !!! a temporary modify for large DOM !!!
         ts = caas_lite.TiffSpliter(geotiff_path, 2000, 2000)
-        tif = tf.TiffFile(ts.tif_path)
 
         if polygon == 'all':
-            data = tif.asarray().astype(float)
-            data[data == geo_head['nodata']] = np.nan
+            data = tif.pages[0].asarray()
             z_mean = np.nanmean(data)
         else:
             if isinstance(polygon, np.ndarray):
@@ -296,21 +204,14 @@ def min_values(geotiff_path, polygon='all', geo_head=None, pctl=5):
     :param geo_head:
     :return:
     """
-    with tifffile.TiffFile(geotiff_path) as tif:
-        # equal to get_header()
-        if geo_head is None:
-            geo_head = _prase_header_string(tif.info())
-        # equal to get_imarray()
-        #data = tif.asarray().astype(float)
-        #data[data == geo_head['nodata']] = np.nan
+    with tf.TiffFile(geotiff_path) as tif:
+        geo_head = get_header(geotiff_path)
 
         # !!! a temporary modify for large DOM !!!
         ts = caas_lite.TiffSpliter(geotiff_path, 2000, 2000)
-        tif = tf.TiffFile(ts.tif_path)
 
         if polygon == 'all':
-            data = tif.asarray().astype(float)
-            data[data == geo_head['nodata']] = np.nan
+            data = tif.pages[0].asarray()
             z_min = percentile_values(data, pctl)
         else:
             if isinstance(polygon, np.ndarray):
@@ -439,6 +340,29 @@ def imarray_clip(imarray, polygon_hv):
 
     roi_rm_offset = polygon_hv - roi_offset
 
+    # in scikit-image 0.18.3, the polygon will generate index outside the image
+    # this will cause out of index error in the `mask[rr, cc] = 1.0`
+    # so need to find out the point locates on the maximum edge and minus 1 
+    # >>> a = np.array([217, 468])  # roi_max
+    # >>> b = np.asarray([[217, 456],[30, 468],[0, 12],[187, 0],[217,456]]) # roi
+    # >>> b
+    # array([[217, 456],
+    #        [ 30, 468],
+    #        [  0,  12],
+    #        [187,   0],
+    #        [217, 456]])
+    # >>> b[:,0] == a[0]
+    # array([ True, False, False, False,  True])
+    # >>> b[b[:,0] == a[0], 0] -= 1
+    # >>> b
+    # array([[216, 456],
+    #        [ 30, 468],
+    #        [  0,  12],
+    #        [187,   0],
+    #        [216, 456]])
+    roi_rm_offset[roi_rm_offset[:,0] == roi_max[0], 0] -= 1
+    roi_rm_offset[roi_rm_offset[:,1] == roi_max[1], 1] -= 1
+
     dim = len(imarray.shape)
 
     if dim == 2: # only has 2 dimensions, DSM 1 band only, other value outside polygon = np.nan
@@ -494,36 +418,33 @@ def clip_roi(roi_polygon_hv, geotiff, is_geo=False, geo_head=None):
     """
     roi_list = _is_roi_type(roi_polygon_hv)
 
-    if geo_head is None:
-        geo_head = get_header(geotiff)
 
-    if not isinstance(geotiff, str):   # give a file_path of geo_tiff
-    #    dxm = get_imarray(geotiff, geo_head)
-    #elif isinstance(geotiff, np.ndarray):
-    #    dxm = geotiff
-    #else:
-        raise TypeError('Invalid geotiff type, either geotiff_file_path or read ndarray by geotiff.get_imarray() function')
+    if not isinstance(geotiff, str):
+        raise TypeError('Invalid geotiff type, either geotiff_file_path string [Todo] or read ndarray by geotiff.get_imarray() function')
 
     offsets = []
     imarrays = []
 
-    # !!! a temporary modify for large DOM !!!
-    ts = caas_lite.TiffSpliter(geotiff, 2000, 2000)
-    tif = tf.TiffFile(ts.tif_path)
+    with tf.TiffFile(geotiff) as tif:
+        if geo_head is None:
+            geo_head = get_header(geotiff)
+            print(geo_head)
 
-    for roi in roi_list:
-        if is_geo:
-            roi_pix = geo2pixel(roi, geo_head)   # (horizontal, vertical)
-        else:
-            roi_pix = roi
+        # !!! a temporary modify for large DOM !!!
+        ts = caas_lite.TiffSpliter(geotiff, 2000, 2000)
 
-        #imarray_out, offset_out = imarray_clip(dxm, roi_geo)
-        imarray_tmp, coord_np_off, offset_out = crop_by_coord(geotiff, roi_pix, buffer=0, ts=ts, tif=tif)
+        for roi in roi_list:
+            if is_geo:
+                roi_pix = geo2pixel(roi, geo_head)   # (horizontal, vertical)
+            else:
+                roi_pix = roi
 
-        imarray_out, _ = imarray_clip(imarray_tmp, coord_np_off)
+            #imarray_out, offset_out = imarray_clip(dxm, roi_geo)
+            imarray_tmp, coord_np_off, offset_out = crop_by_coord(geotiff, roi_pix, buffer=0, ts=ts, tif=tif)
+            imarray_out, _ = imarray_clip(imarray_tmp, coord_np_off)
 
-        imarrays.append(imarray_out)
-        offsets.append(offset_out)
+            imarrays.append(imarray_out)
+            offsets.append(offset_out)
 
     return imarrays, offsets
 
